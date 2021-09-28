f2c0dcea7cda92172452fae16dc061bbfda93816
Adrianna Łuczak <noalterna@gmail.com>
2021-09-28 15:03:35 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="App-logo">
+          Adrianna Łuczak
+        </p>
+        <p className="about">
+          About
+        </p>
+      </header>
+      <div>
+        <p>1</p>
+        <div></div>
+        <p>2</p>
+        <div></div>
+        <p>3</p>
+        <div></div>
+        </div>
+      <div>
+        <p>Project Title</p>
+        <p>Project brief</p>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
